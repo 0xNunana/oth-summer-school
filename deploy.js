30454a8ba89fe170db9c9ebbbc487c4f99bbcd9e
0xNunana <kudayapaul@gmail.com>
2026-08-20 21:17:54 +0000

    Set client.setMaxAttempts(1) to prevent Duplicate Transaction errors on deploy

--- deploy.js
+++ deploy.js
@@ -145,6 +145,11 @@ function makeTestnetClient() {
   client.setOperator(AccountId.fromString(operatorId), operatorKey);
   // Cap accidental fee spend; adjust as needed.
   client.setDefaultMaxTransactionFee(new Hbar(20));
+  
+  // Prevent the SDK from automatically retrying transactions if a node is slow.
+  // This prevents the DUPLICATE_TRANSACTION error from occurring during ContractCreateFlow.
+  client.setMaxAttempts(1);
+  
   return client;
 }
 
